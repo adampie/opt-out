8a55906cd49a90bcb2c1a8211e819e4f817c7e1e
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:10:36 +0000

    Exclude intellij-idea: settings-file opt-out only (#225)

---
+++ tools/_intellij-idea.nix
@@ -0,0 +1,13 @@
+{
+  name = "intellij-idea";
+  meta = {
+    description = "JetBrains IntelliJ IDEA Java and Kotlin IDE";
+    homepage = "https://www.jetbrains.com/idea/";
+    documentation = "https://www.jetbrains.com/help/idea/settings-usage-statistics.html";
+    lastChecked = "2026-03-14";
+    hasTelemetry = true;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
