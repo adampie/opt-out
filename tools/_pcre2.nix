dfe480fc9cd3120ef9fbb3673b48719db11769bd
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:56:17 +0000

    Exclude readline: no telemetry, GNU input library (#204)

---
+++ tools/_pcre2.nix
@@ -0,0 +1,13 @@
+{
+  name = "pcre2";
+  meta = {
+    description = "Perl Compatible Regular Expressions library";
+    homepage = "https://github.com/PCRE2Project/pcre2";
+    documentation = "https://github.com/PCRE2Project/pcre2";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
