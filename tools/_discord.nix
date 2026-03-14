4b3f159aef831e666e653453b54b6646731f2c0e
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:12:13 +0000

    Exclude discord: no env var telemetry opt-out (#221)

---
+++ tools/_discord.nix
@@ -0,0 +1,13 @@
+{
+  name = "discord";
+  meta = {
+    description = "A proprietary voice, video, and text communication platform";
+    homepage = "https://discord.com";
+    documentation = "https://discord.com/privacy";
+    lastChecked = "2026-03-14";
+    hasTelemetry = true;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
