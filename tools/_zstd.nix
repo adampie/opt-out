6df228e3ad09da66cc9b53d537bb8dcb934194ab
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:24:20 +0000

    Exclude zstd: no telemetry (#213)

---
+++ tools/_zstd.nix
@@ -0,0 +1,13 @@
+{
+  name = "zstd";
+  meta = {
+    description = "Zstandard fast real-time compression algorithm and CLI tool";
+    homepage = "https://github.com/facebook/zstd";
+    documentation = "https://github.com/facebook/zstd";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
