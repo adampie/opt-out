3eadbf9ecef59eb6fe3142a9935a42659bb5599b
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:53:07 +0000

    Exclude uvwasi: no telemetry (#210)

---
+++ tools/_uvwasi.nix
@@ -0,0 +1,13 @@
+{
+  name = "uvwasi";
+  meta = {
+    description = "WASI syscall API built on top of libuv";
+    homepage = "https://github.com/nodejs/uvwasi";
+    documentation = "https://github.com/nodejs/uvwasi";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
