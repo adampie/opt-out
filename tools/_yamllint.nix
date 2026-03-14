38e9f10d45b70934ad8f977f06bae960e90e33de
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:37:45 +0000

    Exclude yamllint: no telemetry found (#169)

---
+++ tools/_yamllint.nix
@@ -0,0 +1,12 @@
+{
+  name = "yamllint";
+  meta = {
+    description = "A linter for YAML files that checks for syntax errors, key repetition, and cosmetic issues.";
+    homepage = "https://github.com/adrienverge/yamllint";
+    documentation = "https://yamllint.readthedocs.io/en/stable/";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
