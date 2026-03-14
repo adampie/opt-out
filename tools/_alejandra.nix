20793aecd509dd7a1f093ef8a7037a2f37d41622
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:29:13 +0000

    Exclude alejandra: no telemetry environment variables
    Investigated alejandra (https://github.com/kamadorueda/alejandra) and
    confirmed it has no telemetry, analytics, or crash reporting. It displays
    local sponsor/contributor messages in terminal output but collects no data.

---
+++ tools/_alejandra.nix
@@ -0,0 +1,12 @@
+{
+  name = "alejandra";
+  meta = {
+    description = "Nix code formatter focused on an opinionated, consistent style.";
+    homepage = "https://github.com/kamadorueda/alejandra";
+    documentation = "https://github.com/kamadorueda/alejandra";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
