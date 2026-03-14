9231a9f722f73b89f16d06505b5121c10d68dc59
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 17:19:21 +0000

    Exclude heptiolabs healthcheck, creasty defaults, chainguard git-urls, tablewriter, Masterminds semver: no telemetry (#255)

---
+++ tools/_heptiolabs-healthcheck.nix
@@ -0,0 +1,13 @@
+{
+  name = "heptiolabs-healthcheck";
+  meta = {
+    description = "Go health check library for Kubernetes liveness and readiness probes";
+    homepage = "https://github.com/heptiolabs/healthcheck";
+    documentation = "https://pkg.go.dev/github.com/heptiolabs/healthcheck";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
