c6e017c31a09b9f7e883c3947f911050db4b1a3c
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 15:43:41 +0000

    Exclude spf13 (Cobra, Viper, pflag): no telemetry
    Investigated all three spf13 Go libraries. None implement telemetry,
    analytics, or crash reporting, so there are no environment variables
    to opt out of.
    
    Closes #91

---
+++ tools/_cobra.nix
@@ -0,0 +1,13 @@
+{
+  name = "cobra";
+  meta = {
+    description = "A library for building powerful modern CLI applications in Go";
+    homepage = "https://github.com/spf13/cobra";
+    documentation = "https://github.com/spf13/cobra";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
