93e8e1aff595343543cbe5736e5b0521ff474930
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:34:56 +0000

    Exclude nixfmt-rfc-style: no telemetry found (#181)

---
+++ tools/_nixfmt-rfc-style.nix
@@ -0,0 +1,12 @@
+{
+  name = "nixfmt-rfc-style";
+  meta = {
+    description = "The official formatter for Nix code, implementing the standard Nix format.";
+    homepage = "https://github.com/NixOS/nixfmt";
+    documentation = "https://github.com/NixOS/nixfmt/blob/master/README.md";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
