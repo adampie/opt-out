e4b1531178777dbcc7ebe80892e7b65e51bb7be0
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 16:17:23 +0000

    Exclude c-ares, brotli, ca-certificates, ada-url: no telemetry (#252)

---
+++ tools/_ada-url.nix
@@ -0,0 +1,13 @@
+{
+  name = "ada-url";
+  meta = {
+    description = "WHATWG-compliant and fast URL parser written in modern C++";
+    homepage = "https://github.com/ada-url/ada";
+    documentation = "https://github.com/ada-url/ada/blob/main/README.md";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
