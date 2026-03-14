49a586780cafd811e06fd51b7b2a5c829af664fe
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:58:24 +0000

    Exclude libngtcp2: no telemetry (#195)

---
+++ tools/_libngtcp2.nix
@@ -0,0 +1,13 @@
+{
+  name = "libngtcp2";
+  meta = {
+    description = "QUIC protocol C library";
+    homepage = "https://github.com/ngtcp2/ngtcp2";
+    documentation = "https://github.com/ngtcp2/ngtcp2";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
