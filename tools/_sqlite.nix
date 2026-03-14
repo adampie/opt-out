852e8f9523c9b5e7eb4cfd4499da1afd6d9c42b7
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:55:54 +0000

    Exclude tmux: no telemetry (#208)

---
+++ tools/_sqlite.nix
@@ -0,0 +1,13 @@
+{
+  name = "sqlite";
+  meta = {
+    description = "Embedded relational database engine";
+    homepage = "https://sqlite.org";
+    documentation = "https://sqlite.org/about.html";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
