c219e609f3f938c3f98b0c6dd8a18b8437bf4096
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 16:39:00 +0000

    Exclude pkg/errors, tint, kr/pretty, flatten, go-flags: no telemetry (#254)

---
+++ tools/_go-flags.nix
@@ -0,0 +1,13 @@
+{
+  name = "go-flags";
+  meta = {
+    description = "Go command line option parser";
+    homepage = "https://github.com/jessevdk/go-flags";
+    documentation = "https://pkg.go.dev/github.com/jessevdk/go-flags";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
---
+++ tools/_kr-pretty.nix
@@ -0,0 +1,13 @@
+{
+  name = "kr-pretty";
+  meta = {
+    description = "Pretty printing for Go values";
+    homepage = "https://github.com/kr/pretty";
+    documentation = "https://pkg.go.dev/github.com/kr/pretty";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
