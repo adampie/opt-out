01c8cadff8cd3f94e30244be49161c1e7d9e97aa
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 16:22:53 +0000

    Exclude colorama, pymdown-extensions, mkdocs-material, validate, gongs: no telemetry (#143 #142 #141 #124 #123)

---
+++ tools/_colorama.nix
@@ -0,0 +1,13 @@
+{
+  name = "colorama";
+  meta = {
+    description = "Cross-platform colored terminal text in Python";
+    homepage = "https://github.com/tartley/colorama";
+    documentation = "https://github.com/tartley/colorama/blob/master/README.rst";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
---
+++ tools/_gongs.nix
@@ -0,0 +1,13 @@
+{
+  name = "gongs";
+  meta = {
+    description = "Thin wrapper around NATS Jetstream client using Go generics";
+    homepage = "https://github.com/sl1pm4t/gongs";
+    documentation = "https://github.com/sl1pm4t/gongs/blob/main/README.md";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
