2f3868e07ca66d9f1c8954e2af4462c6e3e7c22a
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 15:53:48 +0000

    Exclude opentelemetry-go: no self-telemetry (#249)

---
+++ tools/_pflag.nix
@@ -0,0 +1,13 @@
+{
+  name = "pflag";
+  meta = {
+    description = "A drop-in replacement for Go's flag package implementing POSIX/GNU-style flags";
+    homepage = "https://github.com/spf13/pflag";
+    documentation = "https://github.com/spf13/pflag";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
