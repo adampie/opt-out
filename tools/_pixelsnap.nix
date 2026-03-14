5630cfaf76068f53b52422687e9087be35491aea
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:06:52 +0000

    Exclude pixelsnap: no telemetry (#230)

---
+++ tools/_pixelsnap.nix
@@ -0,0 +1,13 @@
+{
+  name = "pixelsnap";
+  meta = {
+    description = "Screen measurement tool for macOS";
+    homepage = "https://pixelsnap.com";
+    documentation = "https://pixelsnap.com/privacy";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
