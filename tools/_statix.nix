37cf22ecdfb380b64338588f05ddebca7327747a
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:37:17 +0000

    Exclude statix: no telemetry found (#183)

---
+++ tools/_statix.nix
@@ -0,0 +1,12 @@
+{
+  name = "statix";
+  meta = {
+    description = "Linter and suggestions tool for the Nix programming language";
+    homepage = "https://github.com/oppiliappan/statix";
+    documentation = "https://github.com/oppiliappan/statix";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
