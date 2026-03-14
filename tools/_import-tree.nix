7f8e0c67eef100ffbcd3ab9687ed5b4959ce14b1
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:22:52 +0000

    Exclude import-tree: no telemetry
    import-tree is a pure Nix library with no executable binary or network
    calls. Investigated and confirmed hasTelemetry=false.

---
+++ tools/_import-tree.nix
@@ -0,0 +1,12 @@
+{
+  name = "import-tree";
+  meta = {
+    description = "A Nix library that recursively imports all .nix files from a directory tree into a single module.";
+    homepage = "https://github.com/vic/import-tree";
+    documentation = "https://github.com/vic/import-tree";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
