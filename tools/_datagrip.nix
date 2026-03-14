8500d0caf9f73bb57168bfa8150309159965ad6f
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:12:03 +0000

    Exclude datagrip: settings-file opt-out only (#220)

---
+++ tools/_datagrip.nix
@@ -0,0 +1,13 @@
+{
+  name = "datagrip";
+  meta = {
+    description = "JetBrains DataGrip database IDE";
+    homepage = "https://www.jetbrains.com/datagrip/";
+    documentation = "https://www.jetbrains.com/help/datagrip/settings-usage-statistics.html";
+    lastChecked = "2026-03-14";
+    hasTelemetry = true;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
