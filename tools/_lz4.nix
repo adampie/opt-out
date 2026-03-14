a5ae59c346844e1c9e69eb44f803d9529bb8c2ed
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:57:41 +0000

    Exclude lz4: no telemetry (#198)

---
+++ tools/_lz4.nix
@@ -0,0 +1,13 @@
+{
+  name = "lz4";
+  meta = {
+    description = "Extremely fast lossless compression algorithm";
+    homepage = "https://github.com/lz4/lz4";
+    documentation = "https://github.com/lz4/lz4";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
