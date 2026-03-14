837e7c8fc871c716be5ced22b3bf092b5b4ddb1d
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:50:51 +0000

    Exclude xz: no telemetry (#211)

---
+++ tools/_xz.nix
@@ -0,0 +1,13 @@
+{
+  name = "xz";
+  meta = {
+    description = "XZ Utils lossless data compression tools and library";
+    homepage = "https://tukaani.org/xz/";
+    documentation = "https://tukaani.org/xz/";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
