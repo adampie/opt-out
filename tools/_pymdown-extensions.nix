35930dc90e1dced996f12bcbb86d5b91e1c412b0
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 16:29:02 +0000

    Exclude colorama, pymdown-extensions, mkdocs-material, validate, gongs: no telemetry (#253)

---
+++ tools/_pymdown-extensions.nix
@@ -0,0 +1,13 @@
+{
+  name = "pymdown-extensions";
+  meta = {
+    description = "Extensions for Python-Markdown";
+    homepage = "https://github.com/facelessuser/pymdown-extensions";
+    documentation = "https://facelessuser.github.io/pymdown-extensions/";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
