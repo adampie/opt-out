47c33662c51078133e92e2718d9f9648ea044b49
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 10:38:21 +0000

    Exclude nixd: no telemetry environment variables (#172)

---
+++ tools/_nixd.nix
@@ -0,0 +1,12 @@
+{
+  name = "nixd";
+  meta = {
+    description = "Nix language server with rich IDE features powered by Nix libraries";
+    homepage = "https://github.com/nix-community/nixd";
+    documentation = "https://github.com/nix-community/nixd";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+}
