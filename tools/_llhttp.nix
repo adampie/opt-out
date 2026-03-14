0f912973fc36b7c13e01f7d932c21cafde980791
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-14 14:58:06 +0000

    Exclude llhttp: no telemetry (#197)

---
+++ tools/_llhttp.nix
@@ -0,0 +1,13 @@
+{
+  name = "llhttp";
+  meta = {
+    description = "HTTP/1.1 parser library used by Node.js";
+    homepage = "https://github.com/nodejs/llhttp";
+    documentation = "https://github.com/nodejs/llhttp";
+    lastChecked = "2026-03-14";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
