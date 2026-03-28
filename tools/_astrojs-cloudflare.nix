ce1f1ba25d11da30bb7637f0402e0180dc779fd5
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 23:09:37 +0000

    Exclude @astrojs/cloudflare: no telemetry (#310)

---
+++ tools/_astrojs-cloudflare.nix
@@ -0,0 +1,13 @@
+{
+  name = "astrojs-cloudflare";
+  meta = {
+    description = "Astro adapter for deploying to Cloudflare";
+    homepage = "https://github.com/withastro/adapters";
+    documentation = "https://github.com/withastro/adapters";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
