f0da73885ecfaa99aeba1c9794d91322fb4c04a8
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:18:50 +0000

    Exclude vitest: no telemetry (#272)

---
+++ tools/_vitest.nix
@@ -0,0 +1,13 @@
+{
+  name = "vitest";
+  meta = {
+    description = "Next generation testing framework";
+    homepage = "https://github.com/vitest-dev/vitest";
+    documentation = "https://github.com/vitest-dev/vitest";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
