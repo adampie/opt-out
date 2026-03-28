3033914b59692f8f1d7a727cbe429f248c46e9ad
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:24:58 +0000

    Exclude lucide-react: no telemetry (#66)

---
+++ tools/_lucide-react.nix
@@ -0,0 +1,13 @@
+{
+  name = "lucide-react";
+  meta = {
+    description = "Beautiful and consistent icon library";
+    homepage = "https://github.com/lucide-icons/lucide";
+    documentation = "https://github.com/lucide-icons/lucide";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
