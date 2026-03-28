846581455b7a481953dc281f428412c7171a1131
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 23:12:51 +0000

    Exclude gofumpt: no telemetry (#312)

---
+++ tools/_gofumpt.nix
@@ -0,0 +1,13 @@
+{
+  name = "gofumpt";
+  meta = {
+    description = "Stricter gofmt-compatible Go code formatter";
+    homepage = "https://github.com/mvdan/gofumpt";
+    documentation = "https://github.com/mvdan/gofumpt";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
