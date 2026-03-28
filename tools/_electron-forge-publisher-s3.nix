ee1959292739687aaf38088b0f6f441ca619bc0d
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:52:38 +0000

    Exclude @electron-forge/publisher-s3: no telemetry (#300)

---
+++ tools/_electron-forge-publisher-s3.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-publisher-s3";
+  meta = {
+    description = "Electron Forge publisher for uploading artifacts to Amazon S3";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
