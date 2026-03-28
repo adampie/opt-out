eb127bb1ff68cdc6a608e5ec381735c7a3610dbe
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:32:42 +0000

    Exclude class-variance-authority: no telemetry (#61)

---
+++ tools/_class-variance-authority.nix
@@ -0,0 +1,13 @@
+{
+  name = "class-variance-authority";
+  meta = {
+    description = "Class variance authority for creating variant-based CSS class utilities";
+    homepage = "https://github.com/joe-bell/cva";
+    documentation = "https://github.com/joe-bell/cva";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
