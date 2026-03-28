9496285654972facb7e4cd246cdf2fbfdc1ee748
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:52:29 +0000

    Exclude @electron/fuses: no telemetry (#299)

---
+++ tools/_electron-fuses.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-fuses";
+  meta = {
+    description = "Library for flipping Electron fuses to enable or disable features";
+    homepage = "https://github.com/electron/fuses";
+    documentation = "https://github.com/electron/fuses";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
