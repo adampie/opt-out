d5416532dede9bb14bbfe54904e7a2ab2a8af2e5
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:39:49 +0000

    Exclude @testing-library/jest-dom: no telemetry (#54)

---
+++ tools/_testing-library-jest-dom.nix
@@ -0,0 +1,13 @@
+{
+  name = "testing-library-jest-dom";
+  meta = {
+    description = "Custom Jest matchers for testing the state of the DOM";
+    homepage = "https://github.com/testing-library/jest-dom";
+    documentation = "https://github.com/testing-library/jest-dom";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
