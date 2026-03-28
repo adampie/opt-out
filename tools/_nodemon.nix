b989bf83bb164c464f683fc791e75833deeae4eb
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:26:04 +0000

    Exclude nodemon: no telemetry (#280)

---
+++ tools/_nodemon.nix
@@ -0,0 +1,13 @@
+{
+  name = "nodemon";
+  meta = {
+    description = "Monitor for changes in Node.js applications and automatically restart the server";
+    homepage = "https://github.com/remy/nodemon";
+    documentation = "https://github.com/remy/nodemon";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
