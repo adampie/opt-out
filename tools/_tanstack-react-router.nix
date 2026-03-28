f48bea6bb4521d1a8ca84ccd84feb17209a73e24
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:43:45 +0000

    Exclude @tanstack/react-router: no telemetry (#297)

---
+++ tools/_tanstack-react-router.nix
@@ -0,0 +1,13 @@
+{
+  name = "tanstack-react-router";
+  meta = {
+    description = "Type-safe routing library with built-in data fetching";
+    homepage = "https://github.com/TanStack/router";
+    documentation = "https://github.com/TanStack/router";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
