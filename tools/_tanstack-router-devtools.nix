6aaa02908d3d8c24a53a63213359a3eadd63f7ed
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:41:07 +0000

    Exclude @tanstack/router-devtools: no telemetry (#52)

---
+++ tools/_tanstack-router-devtools.nix
@@ -0,0 +1,13 @@
+{
+  name = "tanstack-router-devtools";
+  meta = {
+    description = "Developer tools for debugging TanStack Router";
+    homepage = "https://github.com/TanStack/router";
+    documentation = "https://github.com/TanStack/router";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
