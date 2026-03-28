8b2d5355581c449f896a1dcd5f6c7f7d3bb8d7d7
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:18:38 +0000

    Exclude Anthropic SDK Go: no telemetry (#271)

---
+++ tools/_anthropic-sdk-go.nix
@@ -0,0 +1,13 @@
+{
+  name = "anthropic-sdk-go";
+  meta = {
+    description = "Go client library for the Anthropic API";
+    homepage = "https://github.com/anthropics/anthropic-sdk-go";
+    documentation = "https://github.com/anthropics/anthropic-sdk-go";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
