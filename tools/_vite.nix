6a62118022090a72ee418c704b881b843afd5816
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:21:39 +0000

    Exclude vite: no telemetry (#273)

---
+++ tools/_vite.nix
@@ -0,0 +1,13 @@
+{
+  name = "vite";
+  meta = {
+    description = "Next generation frontend build tool";
+    homepage = "https://github.com/vitejs/vite";
+    documentation = "https://github.com/vitejs/vite";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
