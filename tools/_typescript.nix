10be26e25c64861f6710747b7ce61bd9955bc0b7
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:21:45 +0000

    Exclude typescript: no telemetry (#274)

---
+++ tools/_typescript.nix
@@ -0,0 +1,13 @@
+{
+  name = "typescript";
+  meta = {
+    description = "Typed superset of JavaScript that compiles to plain JavaScript";
+    homepage = "https://github.com/microsoft/TypeScript";
+    documentation = "https://github.com/microsoft/TypeScript";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
