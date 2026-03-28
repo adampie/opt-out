701deace2cdccb8fa5e9b75adca9449a8fb99a3c
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 23:03:46 +0000

    Exclude @electron-forge/maker-zip: no telemetry (#305)

---
+++ tools/_electron-forge-maker-zip.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-maker-zip";
+  meta = {
+    description = "Electron Forge maker for creating ZIP archives";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
