41d4f72636d9d4cb551f406777a90e248130edb7
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:25:40 +0000

    Exclude react-dom: no telemetry (#277)

---
+++ tools/_react-dom.nix
@@ -0,0 +1,13 @@
+{
+  name = "react-dom";
+  meta = {
+    description = "Entry point to the DOM and server renderers for React";
+    homepage = "https://github.com/facebook/react";
+    documentation = "https://github.com/facebook/react";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
