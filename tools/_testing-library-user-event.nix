3bd90a74b43555cb18b8567fb4cd80d55c9f71f4
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:43:24 +0000

    Exclude @testing-library/user-event: no telemetry (#292)

---
+++ tools/_testing-library-user-event.nix
@@ -0,0 +1,13 @@
+{
+  name = "testing-library-user-event";
+  meta = {
+    description = "Simulate user events for testing";
+    homepage = "https://github.com/testing-library/user-event";
+    documentation = "https://github.com/testing-library/user-event";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
