7247b2f5d646f32fd23ee440b77231b365151d1a
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:53:09 +0000

    Exclude @electron-forge/plugin-auto-unpack-natives: no telemetry (#304)

---
+++ tools/_electron-forge-plugin-auto-unpack-natives.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-plugin-auto-unpack-natives";
+  meta = {
+    description = "Electron Forge plugin that automatically unpacks native Node modules";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
