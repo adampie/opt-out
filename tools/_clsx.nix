65709b2cdda5bb44bde2e55c0277c4c3970674cd
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:31:35 +0000

    Exclude clsx: no telemetry (#62)

---
+++ tools/_clsx.nix
@@ -0,0 +1,13 @@
+{
+  name = "clsx";
+  meta = {
+    description = "Tiny utility for constructing className strings conditionally";
+    homepage = "https://github.com/lukeed/clsx";
+    documentation = "https://github.com/lukeed/clsx";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
