969278ef4a789f6390bce8737a2707a28005cd4d
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:42:32 +0000

    Exclude @tailwindcss/vite: no telemetry (#50)

---
+++ tools/_tailwindcss-vite.nix
@@ -0,0 +1,13 @@
+{
+  name = "tailwindcss-vite";
+  meta = {
+    description = "Vite plugin for Tailwind CSS";
+    homepage = "https://github.com/tailwindlabs/tailwindcss";
+    documentation = "https://github.com/tailwindlabs/tailwindcss";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
