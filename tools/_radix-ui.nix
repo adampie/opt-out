a9a844b7ca1313263b7530f031b5a49bda815cb9
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:22:33 +0000

    Exclude radix-ui: no telemetry (#69)

---
+++ tools/_radix-ui.nix
@@ -0,0 +1,13 @@
+{
+  name = "radix-ui";
+  meta = {
+    description = "Unstyled, accessible UI component primitives";
+    homepage = "https://github.com/radix-ui/primitives";
+    documentation = "https://github.com/radix-ui/primitives";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
