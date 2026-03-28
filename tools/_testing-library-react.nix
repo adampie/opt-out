c61dc3423a9a3e15345d14ae2946d71ce07603e7
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:39:14 +0000

    Exclude @testing-library/react: no telemetry (#55)

---
+++ tools/_testing-library-react.nix
@@ -0,0 +1,13 @@
+{
+  name = "testing-library-react";
+  meta = {
+    description = "Simple and complete testing utilities for React components";
+    homepage = "https://github.com/testing-library/react-testing-library";
+    documentation = "https://github.com/testing-library/react-testing-library";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
