1b50229f22c6cc738db791cd73bdee652b848095
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:43:19 +0000

    Exclude @types/react: no telemetry (#291)

---
+++ tools/_types-react.nix
@@ -0,0 +1,13 @@
+{
+  name = "types-react";
+  meta = {
+    description = "TypeScript type definitions for React";
+    homepage = "https://github.com/DefinitelyTyped/DefinitelyTyped";
+    documentation = "https://github.com/DefinitelyTyped/DefinitelyTyped";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
