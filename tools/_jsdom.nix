9c7c5817e1c1207908923f9648ad4d8ce728a990
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:25:54 +0000

    Exclude jsdom: no telemetry (#65)

---
+++ tools/_jsdom.nix
@@ -0,0 +1,13 @@
+{
+  name = "jsdom";
+  meta = {
+    description = "JavaScript implementation of various web standards for use with Node.js";
+    homepage = "https://github.com/jsdom/jsdom";
+    documentation = "https://github.com/jsdom/jsdom";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
