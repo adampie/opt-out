020f5bd18972d7df45030075d6f4284290d72a94
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:25:46 +0000

    Exclude react: no telemetry (#278)

---
+++ tools/_react.nix
@@ -0,0 +1,13 @@
+{
+  name = "react";
+  meta = {
+    description = "Library for building user interfaces";
+    homepage = "https://github.com/facebook/react";
+    documentation = "https://github.com/facebook/react";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
