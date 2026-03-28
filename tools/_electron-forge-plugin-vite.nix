2de27bc54b7c01c4901455309ea11d3cec763958
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:46:59 +0000

    Exclude @electron-forge/plugin-vite: no telemetry (#46)

---
+++ tools/_electron-forge-plugin-vite.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-plugin-vite";
+  meta = {
+    description = "Electron Forge plugin for building applications with Vite";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
