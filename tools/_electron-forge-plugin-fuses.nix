f79bb76270941dc7d182c9bf670e5776aa7f270d
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:52:52 +0000

    Exclude @electron-forge/plugin-fuses: no telemetry (#303)

---
+++ tools/_electron-forge-plugin-fuses.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-plugin-fuses";
+  meta = {
+    description = "Electron Forge plugin for configuring Electron fuses";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
