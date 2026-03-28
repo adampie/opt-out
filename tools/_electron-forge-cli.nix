567c6050cb9dc5d2d883c9699f51173262dfc11c
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 23:03:59 +0000

    Exclude @electron-forge/cli: no telemetry (#308)

---
+++ tools/_electron-forge-cli.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-cli";
+  meta = {
+    description = "Command-line interface for Electron Forge";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
