70e3688117f43f3c8055b19fd57d88ba31abd964
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 21:25:15 +0000

    Exclude 1 Go package: no telemetry (#81)

---
+++ tools/_go-gh.nix
@@ -0,0 +1,13 @@
+{
+  name = "go-gh";
+  meta = {
+    description = "Go module for building GitHub CLI extensions";
+    homepage = "https://github.com/cli/go-gh";
+    documentation = "https://pkg.go.dev/github.com/cli/go-gh";
+    lastChecked = "2026-03-26";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
