aff469221c464fbb23ba35816e8305e8d6c4252c
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:27:42 +0000

    Exclude electron-store: no telemetry (#64)

---
+++ tools/_electron-store.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-store";
+  meta = {
+    description = "Simple data persistence for Electron applications";
+    homepage = "https://github.com/sindresorhus/electron-store";
+    documentation = "https://github.com/sindresorhus/electron-store";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
