894039c4e79ea690279a04c7c7d573ef5602f954
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:25:37 +0000

    Exclude tailwind-merge: no telemetry (#276)

---
+++ tools/_tailwind-merge.nix
@@ -0,0 +1,13 @@
+{
+  name = "tailwind-merge";
+  meta = {
+    description = "Utility function to merge Tailwind CSS classes without style conflicts";
+    homepage = "https://github.com/dcastil/tailwind-merge";
+    documentation = "https://github.com/dcastil/tailwind-merge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
