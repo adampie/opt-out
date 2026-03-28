0b4e016214542a8aae9cf1912f045f27668ae1ba
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:46:30 +0000

    Exclude @electron-forge/publisher-github: no telemetry (#47)

---
+++ tools/_electron-forge-publisher-github.nix
@@ -0,0 +1,13 @@
+{
+  name = "electron-forge-publisher-github";
+  meta = {
+    description = "Electron Forge publisher for uploading artifacts to GitHub Releases";
+    homepage = "https://github.com/electron/forge";
+    documentation = "https://github.com/electron/forge";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
