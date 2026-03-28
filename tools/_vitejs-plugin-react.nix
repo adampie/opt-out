09eb97362647544784fe47d9c5586f3bbe5ce126
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-28 22:34:07 +0000

    Exclude @vitejs/plugin-react: no telemetry (#59)

---
+++ tools/_vitejs-plugin-react.nix
@@ -0,0 +1,13 @@
+{
+  name = "vitejs-plugin-react";
+  meta = {
+    description = "Official Vite plugin for React with Fast Refresh support";
+    homepage = "https://github.com/vitejs/vite-plugin-react";
+    documentation = "https://github.com/vitejs/vite-plugin-react";
+    lastChecked = "2026-03-28";
+    hasTelemetry = false;
+  };
+  variables = { };
+  commands = { };
+  config = { };
+}
