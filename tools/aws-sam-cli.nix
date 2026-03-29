b536b177fccf435df92a8897e86ab5cf5e578cab
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-29 17:42:27 +0100

    Add AWS SAM CLI: telemetry opt-out (#341)

---
+++ tools/aws-sam-cli.nix
@@ -0,0 +1,15 @@
+{
+  name = "aws-sam-cli";
+  meta = {
+    description = "AWS Serverless Application Model command line interface";
+    homepage = "https://github.com/aws/aws-sam-cli";
+    documentation = "https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-telemetry.html";
+    lastChecked = "2026-03-29";
+    hasTelemetry = true;
+  };
+  variables = {
+    SAM_CLI_TELEMETRY = "0";
+  };
+  commands = { };
+  config = { };
+}
