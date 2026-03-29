d5df7ad0c1441e3d09253cbc1dc1a8f8b3840d31
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-29 17:42:21 +0100

    Add Flox: telemetry opt-out (#321)
    Closes #321

---
+++ tools/flox.nix
@@ -0,0 +1,15 @@
+{
+  name = "flox";
+  meta = {
+    description = "Flox virtual environment and package manager";
+    homepage = "https://github.com/flox/flox";
+    documentation = "https://flox.dev/docs/man/flox-config/";
+    lastChecked = "2026-03-29";
+    hasTelemetry = true;
+  };
+  variables = {
+    FLOX_DISABLE_METRICS = "true";
+  };
+  commands = { };
+  config = { };
+}
