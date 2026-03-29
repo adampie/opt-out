7c31c1cf60f20cf38451e5d26f9ba76cdabc0b7e
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-29 17:42:01 +0100

    Add cargo-binstall: telemetry opt-out (#320)
    Closes #320

---
+++ tools/cargo-binstall.nix
@@ -0,0 +1,15 @@
+{
+  name = "cargo-binstall";
+  meta = {
+    description = "Binary installation for Rust projects";
+    homepage = "https://github.com/cargo-bins/cargo-binstall";
+    documentation = "https://github.com/cargo-bins/cargo-binstall#readme";
+    lastChecked = "2026-03-29";
+    hasTelemetry = true;
+  };
+  variables = {
+    BINSTALL_DISABLE_TELEMETRY = "true";
+  };
+  commands = { };
+  config = { };
+}
